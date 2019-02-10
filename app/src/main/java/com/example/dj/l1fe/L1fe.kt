9e73cf43d4d79aebb84f95ca17422db117ad4fd7
DJ <lynesdan>
2019-02-09 23:31:25 -0500

    First commit

---
+++ app/src/main/java/com/example/dj/l1fe/L1fe.kt
@@ -0,0 +1,12 @@
+package com.example.dj.l1fe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class L1fe : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_l1fe)
+    }
+}
